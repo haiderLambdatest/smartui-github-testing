9b9448f87118dd4c098490aaf34fe69e907591fb
Haider Khan <haiderk@lambdatest.com>
2024-04-25 22:59:25 +0530

    stagePR-1

--- tests/test.js
+++ tests/test.js
@@ -37,7 +37,7 @@ async function searchTextOnGoogle() {
     accessKey: KEY,
     name: "test session", // name of the test
     build: platform + browserName + version, // name of the build
-    "smartUI.build": "[PR: 6208]",
+    "smartUI.build": "[!@#$%^{}[]<>?",
     "smartUI.project": "[!@#$%^{}[1]",
     github: {
       url: process.env.GITHUB_URL,
