05a302a118e3119dd63218a421007881c3955c7d
Haider Khan <haiderk@lambdatest.com>
2024-04-25 17:26:56 +0530

    Running test-1

--- tests/test.js
+++ tests/test.js
@@ -36,7 +36,7 @@ async function searchTextOnGoogle() {
     name: "test session", // name of the test
     build: platform + browserName + version, // name of the build
     "smartUI.build": "[PR: 6208]",
-    "smartUI.project": "github-integration",
+    "smartUI.project": "github-integration-demo-1",
     github: {
       url: process.env.GITHUB_URL,
     },
